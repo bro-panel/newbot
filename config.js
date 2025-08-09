fe22f25332f4e34b6e6d1feba3dd448d316c7294
bro-panel <imdbro7@gmail.com>
2025-08-09 08:52:29 +0530

    Add files via upload

---
+++ config.js
@@ -0,0 +1,19 @@
+const fs = require("fs");
+if (fs.existsSync("config.env"))
+  require("dotenv").config({ path: "./config.env" });
+
+function convertToBool(text, fault = "true") {
+  return text === fault ? true : false;
+}
+
+module.exports = {
+  SESSION_ID: process.env.SESSION_ID || "UVgS0QgQ#O8ragKp_axpQCQ2HZywVosBRfHZ3UzTAfmzaeKGg7D0",
+  OWNER_NUM: process.env.OWNER_NUM || "94703403671",
+  PREFIX: process.env.PREFIX || ".",
+  AUTO_READ_STATUS: process.env.AUTO_READ_STATUS || "true",
+  AUTO_REACT_STATUS: process.env.AUTO_REACT_STATUS || "true",
+  MODE : process.env.MODE || "public", 
+  AUTO_STATUS_LIKE: process.env.AUTO_STATUS_LIKE || "true", 
+  AUTO_RECORDING: convertToBool(process.env.AUTO_RECORDING || "false"), 
+  ANTI_DELETE: convertToBool(process.env.ANTI_DELETE || "true"),
+};
